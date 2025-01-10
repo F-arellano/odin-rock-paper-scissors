42f7bd58022e6855caf8e40da69f3e0fc6bc71be
F-arellano <pipe.arellano98@gmail.com>
2025-01-10 17:16:37 -0300

    ADD: step 6, playGame fully implemented
    + modifications to playRound due to prior criteria was wrong,
      module operator returned negative values in some cases
    + implementation of playGame using a for loop

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 console.log("hello Odin Project");
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
     const random_value = Math.random();
-    if (random_value < 1/3) return "rock";
-    if (random_value < 2/3) return "paper";
+    if (random_value < 1./3.) return "rock";
+    if (random_value < 2./3.) return "paper";
     return "scissors";
 }
 
@@ -13,30 +11,39 @@ function getHumanChoice() {
     return prompt("rock, paper or scissors?");
 }
 
-function playRound(humanChoice, computerChoice) {
-    // We define an object to map string inputs to numeric values
-    const values = {
-        rock: 0,
-        paper: 1,
-        scissors: 2,
-    }
-    // get the numeric values
-    const humanValue = values[humanChoice.toLowerCase()];
-    const computerValue = values[computerChoice];
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    // Determine the winner
-    if ((humanValue - computerValue) % 3 == 1) {
-        console.log(`You win! ${humanChoice.toLowerCase()} beats ${computerChoice}`);
-        humanScore++;
-    } else if ((humanValue - computerValue) % 3 == 2) {
-        console.log(`You lose this time, ${humanChoice.toLowerCase()} loses against ${computerChoice}`);
-        computerScore++;
-    } else {
-        console.log(`Draw! both get ${computerChoice}`);
+    function playRound(humanChoice, computerChoice) {
+        // We define an object to map string inputs to numeric values
+        const values = {
+            rock: 0,
+            paper: 1,
+            scissors: 2,
+        }
+        // get the numeric values
+        const humanValue = values[humanChoice.toLowerCase()];
+        const computerValue = values[computerChoice];
+        // Determine the winner
+        const criteria = (humanValue - computerValue + 3) % 3
+        if (criteria == 1) {
+            console.log(`You win! ${humanChoice.toLowerCase()} beats ${computerChoice}`);
+            humanScore++;
+        } else if (criteria == 2) {
+            console.log(`You lose this time, ${humanChoice.toLowerCase()} loses against ${computerChoice}`);
+            computerScore++;
+        } else {
+            console.log(`Draw! you get ${humanChoice} and the PC get ${computerChoice}. val: ${criteria % 3}`);
+        }
+    }
+    
+    for (let i=0; i<5; i++) {
+        let newComputer = getComputerChoice()
+        let newHuman = getHumanChoice()
+        playRound(newHuman, newComputer)
+        console.log(`The score is: You ${humanScore} - ${computerScore} computer`)
     }
 }
 
-let test_computer = getComputerChoice()
-let test_human = getHumanChoice()
-playRound(test_human, test_computer)
-
+playGame()
