25af28fc39abfe39fe344cbbcd1b102b9affe2e4
F-arellano <pipe.arellano98@gmail.com>
2025-01-10 16:20:31 -0300

    ADD: getComputerChoice fully implemented
    + function was implemented using math.random

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("hello Odin Project")
+
+function getComputerChoice() {
+    const random_value = Math.random();
+    if (random_value < 1/3) return "rock";
+    if (random_value < 2/3) return "paper";
+    return "scissors";
+}
